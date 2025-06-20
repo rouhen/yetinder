9670790a99171f1c4567d1e447440cfb3eedbe88
Martin Rohan <martin.rohan@jpower8.cz>
2025-06-20 14:04:08 +0200

    feat(core): Configured Tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -71,6 +71,8 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .enablePostCssLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
