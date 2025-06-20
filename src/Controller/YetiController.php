377e7e4a41cbc90049abd5b1f6c26139b3660030
Martin Rohan <martin.rohan@jpower8.cz>
2025-06-20 16:42:27 +0200

    feat(core): Prepared edit/create page for yetis

--- src/Controller/YetiController.php
+++ src/Controller/YetiController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Yeti;
+use App\Form\YetiType;
 use App\Repository\YetiRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class YetiController extends AbstractController
 {
-    #[Route('/', name: 'app_yeti')]
+    #[Route('/', name: 'yeti_list')]
     public function index(YetiRepository $yetiRepository): Response
     {
         $yetis = $yetiRepository->findBy([], ['votes' => 'DESC'], 10) ?: [];
@@ -21,21 +23,61 @@ final class YetiController extends AbstractController
         ]);
     }
 
-    #[Route('/vote/up/{id}', name: 'vote_up', methods: ['GET'])]
+    #[Route('/yeti/new', name: 'yeti_create', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $yeti = new Yeti();
+        $form = $this->createForm(YetiType::class, $yeti);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($yeti);
+            $em->flush();
+
+            $this->addFlash('success', $this->trans('Yeti has been successfully created!'));
+            return $this->redirectToRoute('yeti_edit', ['id' => $yeti->getId()]);
+        }
+
+        return $this->render('yeti/edit.html.twig', [
+            'form' => $form->createView(),
+            'yeti' => $yeti,
+        ]);
+    }
+
+    #[Route('/yeti/{id}/edit', name: 'yeti_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Yeti $yeti, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(YetiType::class, $yeti);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', $this->trans('Yeti has been successfully updated!'));
+            return $this->redirectToRoute('yeti_edit', ['id' => $yeti->getId()]);
+        }
+
+        return $this->render('yeti/edit.html.twig', [
+            'form' => $form->createView(),
+            'yeti' => $yeti,
+        ]);
+    }
+
+    #[Route('/vote/up/{id}', name: 'yeti_vote_up', methods: ['GET'])]
     public function voteUp(Yeti $yeti, EntityManagerInterface $em): Response
     {
         $yeti->setVotes($yeti->getVotes() + 1);
         $em->flush();
 
-        return $this->redirect($referer ?? $this->generateUrl('app_yeti'));
+        return $this->redirect($referer ?? $this->generateUrl('yeti_list'));
     }
 
-    #[Route('/vote/down/{id}', name: 'vote_down', methods: ['GET'])]
+    #[Route('/vote/down/{id}', name: 'yeti_vote_down', methods: ['GET'])]
     public function voteDown(Yeti $yeti, EntityManagerInterface $em): Response
     {
         $yeti->setVotes($yeti->getVotes() - 1);
         $em->flush();
 
-        return $this->redirect($referer ?? $this->generateUrl('app_yeti'));
+        return $this->redirect($referer ?? $this->generateUrl('yeti_list'));
     }
 }
---
+++ src/Form/YetiType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Yeti;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class YetiType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'attr' => [
+                    'placeholder' => 'Enter the name of the Yeti',
+                ],
+                'required' => true
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('height', IntegerType::class, [
+                'label' => 'Height (cm)',
+                'required' => false,
+            ])
+            ->add('weight', IntegerType::class, [
+                'label' => 'Weight (kg)',
+                'required' => false,
+            ])
+            ->add('image', TextType::class, [
+                'label' => 'Image filename',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Yeti::class,
+        ]);
+    }
+}
