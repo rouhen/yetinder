cec9633908943b11d70ba2beb7af2a7f070e7759
Martin Rohan <martin.rohan@jpower8.cz>
2025-06-20 12:40:38 +0200

    feat(core): Added Yetis fixture

---
+++ src/DataFixtures/YetiFixture.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Yeti;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class YetiFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $jsonFile = __DIR__ . '/../../assets/data/YetiFixture.json';
+        if (!file_exists($jsonFile)) {
+            throw new \RuntimeException("Could not find JSON file: $jsonFile");
+        }
+
+        $json = file_get_contents($jsonFile);
+        $data = json_decode($json, true);
+
+        foreach ($data as $row) {
+            $yeti = new Yeti();
+            $yeti->setName($row['name'] ?? null);
+            $yeti->setDescription($row['description'] ?? null);
+            $yeti->setHeight($row['height'] ?? null);
+            $yeti->setWeight($row['weight'] ?? null);
+            $yeti->setImage($row['image'] ?? null);
+
+            $manager->persist($yeti);
+        }
+
+        $manager->flush();
+    }
+}
